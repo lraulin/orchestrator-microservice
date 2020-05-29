af6e2491a33302b99e89c99f786a3ea9b31e5efd
ss-mw <marc.watters@smoothstack.com>
2020-05-29 16:45:24 -0400

    fix(test): Fix 'Illegal State Unable to find a @SpringBootConfiguration' due to mismatched folder structure

--- src/test/java/com/smoothstack/lms/orchestratorservice/OrchestrationServiceApplicationTests.java
+++ src/test/java/com/smoothstack/lms/orchestratorservice/OrchestrationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.smoothstack.december.orchestrationservice;
+package com.smoothstack.lms.orchestratorservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
