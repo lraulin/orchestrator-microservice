c452b106082757e88321bb98b5b1e2091ba6b85c
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-06-04 10:24:00 -0400

    Removed authentication controller and refactored to move the create jwt method into the UserController.

--- src/main/java/com/smoothstack/lms/orchestratorservice/controller/microservice/BorrowerController.java
+++ src/main/java/com/smoothstack/lms/orchestratorservice/controller/microservice/BorrowerController.java
@@ -4,22 +4,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
-import com.smoothstack.lms.orchestratorservice.controller.AuthenticationController;
 import com.smoothstack.lms.orchestratorservice.entity.Book;
 import com.smoothstack.lms.orchestratorservice.entity.BookLoan;
 import com.smoothstack.lms.orchestratorservice.entity.BookLoan.BookLoanId;
 import com.smoothstack.lms.orchestratorservice.entity.LibraryBranch;
-
-
-
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -35,14 +31,15 @@ public class BorrowerController {
     private static final String libraryBranchesUrl = "/branches";
     private static final String availableBooksNotCheckedOutUrl = "/borrowers/{borrowerId}/branches/{branchId}/available-books/";
     private static final String bookLoansUrl = "/borrowers/{borrowerId}/loans";
-    private static final Logger logger = LogManager.getLogger(AuthenticationController.class);
+    private static final Logger logger = LogManager.getLogger(BorrowerController.class);
 
     private static String fullUrl(String s) {
         return baseUrl + s;
     }
 
     @PostMapping(checkOutUrl)
-    void checkOutBook(@PathVariable("bookId") long bookId, @PathVariable("borrowerId") long borrowerId, @PathVariable("branchId") long branchId) {
+    void checkOutBook(@PathVariable("bookId") long bookId, @PathVariable("borrowerId") long borrowerId,
+            @PathVariable("branchId") long branchId) {
         logger.debug("request: bookId={}, borrowerId={}, branchId={}", bookId, borrowerId, branchId);
         BookLoanId id = new BookLoanId(bookId, borrowerId, branchId);
         this.restTemplate.postForObject(fullUrl("/borrowers/book/checkout"), id, BookLoanId.class);
@@ -65,28 +62,24 @@ public class BorrowerController {
     }
 
     @GetMapping(availableBooksNotCheckedOutUrl)
-    public Book[] getAvailableBooksNotCheckedOut(
-            @PathVariable("branchId") long branchId,
+    public Book[] getAvailableBooksNotCheckedOut(@PathVariable("branchId") long branchId,
             @PathVariable("borrowerId") long borrowerId) {
 
         String url = String.format("/borrowers/%s/branches/%s/available-books/", borrowerId, branchId);
 
         logger.debug("request: branchId: {}, borrowerId={}", branchId, borrowerId);
-        ResponseEntity<Book[]> responseEntity = this.restTemplate.getForEntity(fullUrl(url),
-                Book[].class);
+        ResponseEntity<Book[]> responseEntity = this.restTemplate.getForEntity(fullUrl(url), Book[].class);
         logger.debug("response: {}", responseEntity);
         return responseEntity.getBody();
     }
 
     @GetMapping(bookLoansUrl)
-    public BookLoan[] getBookLoans(
-            @PathVariable("borrowerId") long borrowerId) {
+    public BookLoan[] getBookLoans(@PathVariable("borrowerId") long borrowerId) {
 
         String url = String.format("/borrowers/%s/loans", borrowerId);
 
         logger.debug("request: borrowerId={}", borrowerId);
-        ResponseEntity<BookLoan[]> responseEntity = this.restTemplate.getForEntity(fullUrl(url),
-                BookLoan[].class);
+        ResponseEntity<BookLoan[]> responseEntity = this.restTemplate.getForEntity(fullUrl(url), BookLoan[].class);
         logger.debug("response: {}", responseEntity);
         return responseEntity.getBody();
     }
